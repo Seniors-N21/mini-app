5419ae8d3fb0b84be2ca78b91f62d8033210eaa8
elbekdamirov <elbekmain@gmail.com>
2025-06-25 17:46:44 +0500

    elbek

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
+console.log("Hello world");
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
